5c6f1250eaade0e5409da54002e504c6f3df295e
ngoclinh8123 <ngoclinh8123@gmail.com>
2022-12-30 09:40:25 +0700

    first commit

---
+++ product/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    title=models.CharField(max_length=255,default='')
+    slug=models.CharField(max_length=255,default='')
+    description=models.TextField(default='')
+    active=models.BooleanField(default=True)
+
+class Product(models.Model):
+    title=models.CharField(max_length=255,default='')
+    description=models.TextField(default='')
+    active=models.BooleanField(default=True)
+    price=models.IntegerField(default=0)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+
+class Variation(models.Model):
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    title=models.CharField(max_length=255,default='')
+    price=models.IntegerField(default=0)
+    sale_price=models.IntegerField(default=0)
+    inventory=models.IntegerField(default=0)
+    active=models.BooleanField(default=True)
+    
